955a1ac1ba96b04414354c4a080fdf5c115f6d1b
teri-mo <tmohaupt7@gmail.com>
2024-07-14 21:58:54 +0100

    changing the city

--- src/index.js
+++ src/index.js
@@ -99,4 +99,4 @@ function displayForecast(response) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris");
+searchCity("London");
